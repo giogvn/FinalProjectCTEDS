bc5f37096e063ea6372d1da7a6fe01613b607cf3
unknown <giovanitavares@outlook.com>
2022-11-18 13:50:02 -0300

    Adds Project Folder
    Signed-off-by: unknown <giovanitavares@outlook.com>

---
+++ FinanceControl/FinanceControl/ViewModels/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using FinanceControl.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FinanceControl.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public Command LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(OnLoginClicked);
+        }
+
+        private async void OnLoginClicked(object obj)
+        {
+            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
+            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+        }
+    }
+}
