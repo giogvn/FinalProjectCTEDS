fe0a709279c3ce463b90825fc5524929190f937b
unknown <cteds_07>
2022-11-23 20:10:31 -0300

    Creates the iRecord interface
    Signed-off-by: unknown <cteds_07>

---
+++ SaveFirst/Interfaces/IRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveFirst.Interfaces
+{
+    internal interface iRecord
+    {
+        List<Record> ReadAll();
+        void Create(Record newRecord);
+        void Update(Record record);
+        void Delete(string idRecord);
+    }
+}
