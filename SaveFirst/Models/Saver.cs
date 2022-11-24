118472cc807a8d723aa39754cc4b376832b3f273
unknown <cteds_07>
2022-11-23 21:35:22 -0300

    Implements the Saver Model
    Signed-off-by: unknown <cteds_07>

---
+++ SaveFirst/Models/Saver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveFirst.Models
+{
+    internal class Saver
+    {
+        public int IdUser { get; set; }
+        public int PayerId { get; set; }
+        public string Name { get; set; } = String.Empty;    
+        public DateOnly Birthday { get; set; }
+
+        public string Password { get; set; } = String.Empty;
+
+        public string Type { get; set; } = String.Empty;
+
+
+    }
+}
