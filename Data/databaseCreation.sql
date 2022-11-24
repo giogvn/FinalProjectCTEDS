8def699536cb7abcf15cda14efcbf8dec0d24263
Pedro Campos <84058404+pedr-augusto@users.noreply.github.com>
2022-11-24 18:47:49 -0300

    Replaces instances of "User" with "Saver"
    Standardizes the use of a single keyword whenever refereing to the Saver database (with the exception of the attribute user_type, which makes more sense as is.

--- Data/databaseCreation.sql
+++ Data/databaseCreation.sql
@@ -8,7 +8,7 @@ CREATE TABLE Saver(
 
 CREATE TABLE Expense(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_id INTEGER NOT NULL,
+  saver_id INTEGER NOT NULL,
   expense_date DATE,
   value INTEGER NOT NULL,
   expense_type TEXT CHECK(expense_type = "recurrent" OR expense_type = "sporadic"),
@@ -17,7 +17,7 @@ CREATE TABLE Expense(
   number_of_installments INTEGER DEFAULT 1,
   installment_value INTEGER NOT NULL,
   installments_left INTEGER DEFAULT 1,
-  FOREIGN KEY (user_id) REFERENCES User(id)
+  FOREIGN KEY (saver_id) REFERENCES Saver(id)
 );
 
 CREATE TABLE Category(
@@ -42,13 +42,13 @@ CREATE TABLE FinancialProduct(
 
 CREATE TABLE SaverFinancialProduct(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_id INTEGER,
+  saver_id INTEGER,
   financial_product_id INTEGER,
   recurrence TEXT,
   reason TEXT,
   purchase_date DATE,
   number_of_shares INTEGER NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES User(id),
+  FOREIGN KEY (saver_id) REFERENCES Saver(id),
   FOREIGN KEY (financial_product_id) REFERENCES FinancialProduct(id)
 );
 
@@ -64,37 +64,37 @@ CREATE TABLE ExpensePaymentMethod(
 
 CREATE TABLE CreditCard(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_id INTEGER,
+  saver_id INTEGER,
   payment_method_id INTEGER,
   name TEXT DEFAULT "creditCard",
   bank TEXT DEFAULT "undefinedBank",
   invoice_due_date DATE NOT NULL,
   invoice_closing_date DATE NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES User(id),
+  FOREIGN KEY (saver_id) REFERENCES Saver(id),
   FOREIGN KEY (payment_method_id) REFERENCES PaymentMethod(id)
 );
 
 CREATE TABLE CheckingAccount(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_id INTEGER,
+  saver_id INTEGER,
   payment_method_id INTEGER,
   name TEXT DEFAULT "notDefined",
   bank TEXT NOT NULL,
   registration_date DATE NOT NULL,
   cancel_date DATE,
-  FOREIGN KEY (user_id) REFERENCES User(id),
+  FOREIGN KEY (saver_id) REFERENCES Saver(id),
   FOREIGN KEY (payment_method_id) REFERENCES PaymentMethod(id)
 );
 
 CREATE TABLE IncomeResource(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_id INTEGER,
+  saver_id INTEGER,
   value INTEGER NOT NULL,
   payday DATE NOT NULL,
   start_day DATE,
   end_day DATE,
   recurrence TEXT NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES User(id)
+  FOREIGN KEY (saver_id) REFERENCES Saver(id)
 );
 
 CREATE TABLE PaymentMethodIncomeResource(
