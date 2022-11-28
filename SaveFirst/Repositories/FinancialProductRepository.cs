e80bd24694f58399c53baaac9310b20bca7a2194
unknown <giovanitavares@outlook.com>
2022-11-28 17:00:02 -0300

    Creates Tthe FinancialProductRepository that communicates with the Tesouro Direto API
    Signed-off-by: unknown <giovanitavares@outlook.com>

---
+++ SaveFirst/Repositories/FinancialProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using SaveFirst.Interfaces;
+using SaveFirst.Models;
+using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace SaveFirst.Repositories
+{
+    public class FinancialProductRepository : IRecord<FinancialProduct>
+    { 
+        public void Delete(int RecordId) => throw new NotImplementedException();
+
+        public void Create(FinancialProduct newRecord) => throw new NotImplementedException();
+
+        public List<FinancialProduct> FindAllFromSaver(int Id) => throw new NotImplementedException();
+
+        public List<FinancialProduct> ReadAll()
+        {
+            List<FinancialProduct> list = new();
+
+            using (WebClient wc = new())
+            {
+                var json = wc.DownloadString(FinancialProduct.PriceSource);
+                var objects = JObject.Parse(json)["response"]["TrsrBdTradgList"];
+                foreach (JObject obj in objects)
+                {
+                    FinancialProduct record = new FinancialProduct()
+                    {
+                        Name = obj["TrsrBd"]["nm"].ToString(),
+                        Price = (float)obj["TrsrBd"]["minRedVal"]
+                    };
+                    list.Add(record);
+                }
+            }
+            return list;
+        }
+        public void Update(FinancialProduct record) => throw new NotImplementedException();
+    }
+}
