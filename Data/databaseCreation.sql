433c5fb71f166cf2d36bf6ae8d4a9acb0a9f7dcf
unknown <giovanitavares@outlook.com>
2022-12-13 18:26:24 -0300

    Fixes sql file
    Signed-off-by: unknown <giovanitavares@outlook.com>

--- Data/databaseCreation.sql
+++ Data/databaseCreation.sql
@@ -1,26 +1,22 @@
-
-CREATE DATABASE [SaveFirst];
-GO
-
 USE [SaveFirst];
 GO
 
 CREATE TABLE Saver(
-	id INT PRIMARY KEY IDENTITY(1,1),
+	id VARCHAR(255) PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
-  type VARCHAR(255),
-	payer_id INT,
+	type VARCHAR(255),
+	payer_id VARCHAR(255),
 	name	VARCHAR(255) NOT NULL,
-	birthdate DATE
+	birthdate DATETIME
 );
 
 GO
 
 CREATE TABLE Expense(
-  id INT PRIMARY KEY IDENTITY(1,1),
-  saver_id INT NOT NULL,
-  expense_date DATE,
-  due_date DATE,
+  id VARCHAR(255) PRIMARY KEY,
+  saver_id VARCHAR(255) NOT NULL,
+  expense_date DATETIME,
+  due_date DATETIME,
   value FLOAT NOT NULL,
   expense_type VARCHAR(255),
   description VARCHAR(255),
@@ -33,8 +29,8 @@ CREATE TABLE Expense(
 
 GO
 CREATE TABLE Category(
-  id INT PRIMARY KEY IDENTITY(1,1),
-  saver_id INT,
+  id VARCHAR(255) PRIMARY KEY,
+  saver_id VARCHAR(255) NOT NULL,
   name VARCHAR(255) NOT NULL,
   FOREIGN KEY (saver_id) REFERENCES Saver(id)
 );
@@ -42,8 +38,8 @@ CREATE TABLE Category(
 GO
 
 CREATE TABLE ExpenseCategory(
-  expense_id INT,
-  category_id INT,
+  expense_id VARCHAR(255),
+  category_id VARCHAR(255), 
   PRIMARY KEY (expense_id, category_id),
   FOREIGN KEY (expense_id) REFERENCES Expense(id),
   FOREIGN KEY (category_id) REFERENCES Category(id)
@@ -51,31 +47,18 @@ CREATE TABLE ExpenseCategory(
 
 GO
 
-CREATE TABLE SaverFinancialProduct(
-  id INT PRIMARY KEY IDENTITY(1,1),
-  saver_id INT,
-  financial_product_name VARCHAR(255),
-  recurrence VARCHAR(255),
-  reason VARCHAR(255),
-  purchase_date DATE,
-  number_of_shares INT NOT NULL,
-  FOREIGN KEY (saver_id) REFERENCES Saver(id),
-);
-
-GO
-
 CREATE TABLE PaymentMethod(
-  id INT PRIMARY KEY IDENTITY(1,1),
-  saver_id INT,
-  payment_method_id INT,
+  id VARCHAR(255) PRIMARY KEY,
+  saver_id VARCHAR(255) NOT NULL,
+  payment_method_id VARCHAR(255),
   name VARCHAR(255),
   type VARCHAR(255),
   bank VARCHAR(255),
   limit FLOAT,
-  invoice_due_date DATE NOT NULL,
-  invoice_closing_date DATE NOT NULL,
-  registration_date DATE NOT NULL,
-  cancel_date DATE DEFAULT NULL,
+  invoice_due_date INT NOT NULL,
+  invoice_closing_date INT NOT NULL,
+  registration_date DATETIME NOT NULL,
+  cancel_date DATETIME DEFAULT NULL,
   FOREIGN KEY (saver_id) REFERENCES Saver(id),
   FOREIGN KEY (payment_method_id) REFERENCES PaymentMethod(id)
 );
@@ -83,30 +66,9 @@ CREATE TABLE PaymentMethod(
 GO
 
 CREATE TABLE ExpensePaymentMethod(
-  expense_id INT,
-  payment_method_id INT,
-  FOREIGN KEY (payment_method_id) REFERENCES PaymentMethod(id)
-);
-
-GO
-
-CREATE TABLE IncomeResource(
-  id INT PRIMARY KEY IDENTITY(1,1),
-  saver_id INT,
-  value INT NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  payday DATE NOT NULL,
-  start_date DATE,
-  end_date DATE,
-  recurrence VARCHAR(255) NOT NULL,
-  FOREIGN KEY (saver_id) REFERENCES Saver(id)
-);
-
-GO
-
-CREATE TABLE PaymentMethodIncomeResource(
-  payment_method_id INT,
-  income_resource_id INT,
+  expense_id VARCHAR(255),
+  payment_method_id VARCHAR(255),
+  PRIMARY KEY (expense_id, payment_method_id),
   FOREIGN KEY (payment_method_id) REFERENCES PaymentMethod(id),
-  FOREIGN KEY (income_resource_id) REFERENCES IncomeResource(id)
+  FOREIGN KEY (expense_id) REFERENCES Expense(id)
 );
