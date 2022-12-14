d4e09f0a1710acfc53f50fb010fecd546d3206ca
pedr-augusto <p_campos@usp.br>
2022-12-14 00:02:52 -0300

    Adds InvoiceDueDate Register

--- SaveFirst/Views/UserControls/CreditCard.xaml.cs
+++ SaveFirst/Views/UserControls/CreditCard.xaml.cs
@@ -17,6 +17,29 @@ namespace SaveFirst.Views.UserControls
             InitializeComponent();
         }
 
+        private void DueDateBoxChecker(object sender, RoutedEventArgs e)
+        {
+            if (!IsLoaded)
+                return;
+
+            if (Regex.IsMatch(DueDateBox.Text.Trim(), "[^0-9]+"))
+            {
+                MessageBox.Show("Digite apenas numeros");
+                DueDateBox.Text = DueDateBox.Text.Remove(DueDateBox.Text.Length - 1);
+            }
+        }
+        private void ClosingDateBoxChecker(object sender, RoutedEventArgs e)
+        {
+            if (!IsLoaded)
+                return;
+
+            if (Regex.IsMatch(ClosingDateBox.Text.Trim(), "[^0-9]+"))
+            {
+                MessageBox.Show("Digite apenas numeros");
+                ClosingDateBox.Text = ClosingDateBox.Text.Remove(ClosingDateBox.Text.Length - 1);
+            }
+        }
+
         public bool ExpirationDateFormatCheck()
         {
             return Regex.IsMatch (ExpireDateBox.Text.Trim(), "[0-9]{2}/[0-9]{4}");
@@ -24,6 +47,11 @@ namespace SaveFirst.Views.UserControls
         public bool ClosingDateValidValue (out int day)
         {
             return int.TryParse(ClosingDateBox.Text, out day) && day > 0 && day <=31;
+        }
+
+        public bool DueDateValidValue (out int day)
+        {
+            return int.TryParse(DueDateBox.Text, out day) && day > 0 && day <= 31;
         }
         public int[] SplitExpirationDate()
         {
