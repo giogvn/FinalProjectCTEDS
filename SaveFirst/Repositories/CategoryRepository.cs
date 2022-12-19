d7876a3fead8ffbaae891333432b30975899004b
Giovani Tavares <78043475+giogvn@users.noreply.github.com>
2022-12-18 22:44:41 -0300

    Update CategoryRepository.cs

--- SaveFirst/Repositories/CategoryRepository.cs
+++ SaveFirst/Repositories/CategoryRepository.cs
@@ -11,7 +11,7 @@ namespace SaveFirst.Repositories
 {
     public class CategoryRepository : IRecord<Category>
     {
-        static string ConnectionString = "Server=labsoft.pcs.usp.br; Initial Catalog=db_7; User id=usuario_7; pwd=44192792818;";
+        static string ConnectionString = "Server=labsoft.pcs.usp.br; Initial Catalog=db_7; User id=''; pwd='';";
         public void Delete(int RecordId)
         {
             using (SqlConnection con = new(ConnectionString))
